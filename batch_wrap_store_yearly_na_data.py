aed1c03d9cb948e54be320795e81f50728da3fb5
Marc Prange <Marc.Prange@an006.princeton.rdhpcs.noaa.gov>
2024-04-30 08:49:49 -0400

    Plotting notebooks for runoff maps and timeseries

--- batch_wrap_store_yearly_na_data.py
+++ batch_wrap_store_yearly_na_data.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import xarray as xr
 
-for year in range(1990, 2020):
+for year in range(1970, 1990):
     os.system("sbatch "
               "/home/Marc.Prange/work/AR-analysis/batch_wrapper.sh "
              f"{year}")
