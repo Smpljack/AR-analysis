12aae5c61f2e03af364cd01db27d58dc628f9c57
Marc Prange <Marc.Prange@an202.princeton.rdhpcs.noaa.gov>
2024-07-29 09:50:03 -0400

    Minor changes to shell scripts and add cdo monmean script

--- batch_wrapper.sh
+++ batch_wrapper.sh
@@ -4,15 +4,15 @@
 #BATCH --partition=analysis
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=8:00:00
 #SBATCH --account=gfdl_w
-#SBATCH --job-name=composite_precip_mean
+#SBATCH --job-name=ar_day_stat
 #SBATCH --chdir=.
 #SBATCH -o /home/Marc.Prange/work/AR-analysis/logs/%x.o%j
 #SBATCH -e /home/Marc.Prange/work/AR-analysis/logs/%x.e%j
 
 module load conda
-conda activate /home/Marc.Prange/miniconda3/envs/AR_analysis
+conda activate work_python
 
-python processing.py --year=$1 #run_multiprocessing.py --lon_min=$1 --lon_max=$2 --exp_name=$3
+python data_util.py --exp_name=$1 #run_multiprocessing.py --lon_min=$1 --lon_max=$2 --exp_name=$3
--- wrap_dmget.sh
+++ wrap_dmget.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 freq="daily"
-base_path="/archive/Ming.Zhao/awg/2022.03/c192L33_am4p0_amip_HIRESMIP_nudge_wind_1951_2020/gfdl.ncrc4-intel-prod-openmp/pp/"
+base_path="/archive/Ming.Zhao/awg/2022.03/c192L33_am4p0_amip_HIRESMIP_nudge_wind_1day_p2K/gfdl.ncrc4-intel-prod-openmp/pp/"
 land_path="${base_path}land/ts/${freq}/1yr/"
 land_cmip_path="${base_path}land_cmip/ts/${freq}/1yr/"
 atmos_cmip_path="${base_path}atmos_cmip/ts/${freq}/1yr/"
@@ -16,28 +16,28 @@ atmos_cmip_6hr_vars=("ivtx" "ivty")
 river_vars=("rv_o_h2o" "rv_d_h2o")
 obs_vars=("prw" "ivtx" "ivty")
 
-for var in ${obs_vars[@]} ; do
-    dmget ${obs_path}ERA5.{1951..2020}0101-*1231.${var}.nc &
-done
+# for var in ${obs_vars[@]} ; do
+#     dmget ${obs_path}ERA5.{1951..2020}0101-*1231.${var}.nc &
+# done
 
 for var in ${land_cmip_vars[@]} ; do
-    dmget ${land_cmip_path}land_cmip.{1951..1990}0101-*1231.${var}.nc &
+    dmget ${land_cmip_path}land_cmip.{1951..2020}0101-*1231.${var}.nc &
 done
 
 for var in ${land_vars[@]} ; do
-    dmget ${land_path}land.{1951..1990}0101-*1231.${var}.nc &
+    dmget ${land_path}land.{1951..2020}0101-*1231.${var}.nc &
 done
 
 for var in ${atmos_cmip_vars[@]} ; do
-    dmget ${atmos_cmip_path}atmos_cmip.{1951..1990}0101-*1231.${var}.nc &
+    dmget ${atmos_cmip_path}atmos_cmip.{1951..2020}0101-*1231.${var}.nc &
 done
 
 for var in ${atmos_cmip_6hr_vars[@]} ; do
-    dmget ${atmos_cmip_6hr_path}atmos_cmip.{1951..1990}010100-*123123.${var}.nc &
+    dmget ${atmos_cmip_6hr_path}atmos_cmip.{1951..2020}010100-*123123.${var}.nc &
 done
 
 for var in ${river_vars[@]} ; do
-    dmget ${river_path}river.{1951..1990}0101-*1231.${var}.nc &
+    dmget ${river_path}river.{1951..2020}0101-*1231.${var}.nc &
 done
 
 
